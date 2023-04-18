3c2161c3936bf90e991c0b3c47ce739f6ec2863c
Aniket Saxena <saxenaaniket@yahoo.com>
2023-04-18 18:55:44 +0200

    fix: fixed test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-test("renders learn react link", () => {
+test("renders react text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/react/i);
+  expect(element).toBeInTheDocument();
 });
